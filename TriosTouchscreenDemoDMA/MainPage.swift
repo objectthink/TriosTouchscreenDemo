66eef0b895e53ac7c3e163637eb6cbcf295c55d5
stephen eshelman <objectthink@comcast.net>
2016-06-08 06:35:36 -0400

    add temperature icon to dma, add peel up help button to doc demo for help

--- TriosTouchscreenDemoDMA/MainPage.swift
+++ TriosTouchscreenDemoDMA/MainPage.swift
@@ -10,42 +10,83 @@ import UIKit
 
 class MainPage: UIViewController, IMercuryPage
 {
+   @IBOutlet var _temperatureButton: UIButton!
+   @IBOutlet var _positionButton:UIButton!
+   @IBOutlet var _measureButton:UIButton!
+   
+   @IBOutlet var _preloadButton:UIButton!
+   @IBOutlet var _floatButton:UIButton!
+   @IBOutlet var _unlockedButton:UIButton!
+   @IBOutlet var _lockedButton:UIButton!
+   
+   var _isSelected:Bool = false
+   
+   var _app:IMercuryApp!
+   var app:IMercuryApp
+      {
+      get
+      {
+         return _app;
+      }
+      set
+      {
+         _app = newValue
+      }
+   }
+   
+   override func viewDidLoad()
+   {
+      super.viewDidLoad()
+      
+      // Do any additional setup after loading the view.
+      _temperatureButton.setImage((UIImage.init(named:"TempDn")), forState: .Highlighted)
+      _positionButton.setImage((UIImage.init(named:"Position1Dn")), forState: .Highlighted)
+      _measureButton.setImage((UIImage.init(named:"Measure1Dn")), forState: .Highlighted)
+      
+      //_preloadButton.setImage((UIImage.init(named:"PreloadDn")), forState: .Highlighted)
+      //_floatButton.setImage((UIImage.init(named:"FloatDn")), forState: .Highlighted)
+      //_unlockedButton.setImage((UIImage.init(named:"UnlockDn")), forState: .Highlighted)
+      //_lockedButton.setImage((UIImage.init(named:"LockDn")), forState: .Highlighted)
+   }
+   
+   override func didReceiveMemoryWarning()
+   {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   @IBAction func modeRadioClicked(sender: AnyObject)
+   {
+      if _isSelected
+      {
+         _preloadButton.setImage((UIImage.init(named:"PreloadDn")), forState: .Normal)
+         
+      }
+      else
+      {
+         _preloadButton.setImage((UIImage.init(named:"PreloadUp")), forState: .Normal)
+         
+      }
+      
+      _isSelected = !_isSelected
+   }
+   
+   @IBAction func temperatureButtonClicked(sender: AnyObject)
+   {
+      let controller = self.storyboard?.instantiateViewControllerWithIdentifier("TemperaturePage")
+      var page = controller as! IMercuryPage
+      
+      _app.next(&page)// as! IMercuryPage)
+   }
+   
+   /*
+    // MARK: - Navigation
     
-    var _app:IMercuryApp!
-    var app:IMercuryApp
-        {
-        get
-        {
-            return _app;
-        }
-        set
-        {
-            _app = newValue
-        }
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
-    
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    */
+   
 }
--- TriosTouchscreenDemoDMA/MainViewController.swift
+++ TriosTouchscreenDemoDMA/MainViewController.swift
@@ -20,176 +20,181 @@ import UIKit
 
 class MainViewController: UIViewController, IMercuryApp
 {
-    var _currentPage:IMercuryPage!
-    var _instrument:MercuryInstrument!
-    var _pages:Stack<IMercuryPage>
+   @IBOutlet weak var _homeButton: UIButton!
+   
+   var _currentPage:IMercuryPage!
+   var _instrument:MercuryInstrument!
+   var _pages:Stack<IMercuryPage>
+   
+   var instrument:MercuryInstrument
+      {
+      get
+      {
+         return _instrument
+      }
+      set
+      {
+         _instrument = newValue
+      }
+   }
+   
+   required init?(coder aDecoder: NSCoder)
+   {
+      _pages = Stack<IMercuryPage>()
+      
+      super.init(coder: aDecoder)
+   }
+   
+   override func viewDidLoad()
+   {
+      super.viewDidLoad()
+      
+      _homeButton.setImage(UIImage.init(named:"home_pressed"), forState: .Highlighted)
+      
+   }
+   
+   func next(inout page:IMercuryPage)
+   {
+      //let nextPage = (page as! UIViewController)
+      //let currentPageController = childViewControllers.last! as UIViewController
+      
+      //peek and call canClose
+      //then push page and call aboutToOpen, aboutToShow, postShow
+      
+      //set before we add the sybview as prepareforsegue will be
+      //triggered for pages with a container view
+      page.app = self
+      
+      transitionFromPage(_pages.peek(), to: page)
+      
+      _pages.push(page)
+   }
+   
+   @IBAction func backButtonClicked(sender: AnyObject)
+   {
+      back()
+   }
+   
+   @IBAction func homeButtonClicked(sender: AnyObject)
+   {
+      home()
+   }
+   
+   func home()
+   {
+      //peek and call aboutToClose, canClose
+      //pop and call aboutToRemove
+      //peek again and call aboutToShow, postShow
+      
+      if _pages.peek() is MainPage
+      {
+         return
+      }
+      
+      var page:IMercuryPage = _pages.peek()
+      let fromPage:IMercuryPage = page;
+      
+      while(!(page is MainPage))
+      {
+         _pages.pop()
+         page = _pages.peek()
+      }
+      
+      transitionFromPage(fromPage, to: page)
+   }
+   
+   func back()
+   {
+      //peek and call aboutToClose, canClose
+      //pop and call aboutToRemove
+      //peek again and call aboutToShow, postShow
+      
+      if _pages.peek() is MainPage
+      {
+         return
+      }
+      
+      transitionFromPage(_pages.pop(), to: _pages.peek())
+   }
+   
+   func transitionFromPage(
+      from:IMercuryPage,
+      to:IMercuryPage
+      )
+   {
+      let fromController:UIViewController = from as! UIViewController
+      let toController:UIViewController = to as! UIViewController
+      
+      fromController.willMoveToParentViewController(nil)
+      
+      addChildViewController(toController)
+      
+      toController.view.frame = fromController.view.frame
+      
+      transitionFromViewController(
+         fromController,
+         toViewController: toController,
+         duration: 0.25,
+         options: .TransitionCrossDissolve,
+         animations:
+         { () -> Void in
+            // nothing needed here
+         },
+         completion:
+         { (finished) -> Void in
+            fromController.removeFromParentViewController()
+            toController.didMoveToParentViewController(self)
+      })
+   }
+   
+   override func didReceiveMemoryWarning()
+   {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+   {
+      if (segue.identifier == "EmbedSegue")
+      {
+         var page = segue.destinationViewController as! IMercuryPage
+         
+         page.app = self
+         
+         _pages.push(page)
+      }
+   }
+   
+   /*
+    // MARK: - Navigation
     
-    var instrument:MercuryInstrument
-        {
-        get
-        {
-            return _instrument
-        }
-        set
-        {
-            _instrument = newValue
-        }
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
-
-    required init?(coder aDecoder: NSCoder)
-    {
-        _pages = Stack<IMercuryPage>()
-        
-        super.init(coder: aDecoder)
-    }
-    
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()        
-    }
-    
-    func next(inout page:IMercuryPage)
-    {
-        //let nextPage = (page as! UIViewController)
-        //let currentPageController = childViewControllers.last! as UIViewController
-        
-        //peek and call canClose
-        //then push page and call aboutToOpen, aboutToShow, postShow
-        
-        //set before we add the sybview as prepareforsegue will be
-        //triggered for pages with a container view
-        page.app = self
-        
-        transitionFromPage(_pages.peek(), to: page)
-        
-        _pages.push(page)
-    }
-    
-    @IBAction func backButtonClicked(sender: AnyObject)
-    {
-        back()
-    }
-    
-    @IBAction func homeButtonClicked(sender: AnyObject)
-    {
-        home()
-    }
-    
-    func home()
-    {
-        //peek and call aboutToClose, canClose
-        //pop and call aboutToRemove
-        //peek again and call aboutToShow, postShow
-        
-        if _pages.peek() is MainPage
-        {
-            return
-        }
-        
-        var page:IMercuryPage = _pages.peek()
-        let fromPage:IMercuryPage = page;
-        
-        while(!(page is MainPage))
-        {
-            _pages.pop()
-            page = _pages.peek()
-        }
-        
-        transitionFromPage(fromPage, to: page)
-    }
-    
-    func back()
-    {
-        //peek and call aboutToClose, canClose
-        //pop and call aboutToRemove
-        //peek again and call aboutToShow, postShow
-        
-        if _pages.peek() is MainPage
-        {
-            return
-        }
-        
-        transitionFromPage(_pages.pop(), to: _pages.peek())
-    }
-    
-    func transitionFromPage(
-        from:IMercuryPage,
-        to:IMercuryPage
-        )
-    {
-        let fromController:UIViewController = from as! UIViewController
-        let toController:UIViewController = to as! UIViewController
-        
-        fromController.willMoveToParentViewController(nil)
-        
-        addChildViewController(toController)
-        
-        toController.view.frame = fromController.view.frame
-        
-        transitionFromViewController(
-            fromController,
-            toViewController: toController,
-            duration: 0.25,
-            options: .TransitionCrossDissolve,
-            animations:
-            { () -> Void in
-                // nothing needed here
-            },
-            completion:
-            { (finished) -> Void in
-                fromController.removeFromParentViewController()
-                toController.didMoveToParentViewController(self)
-        })
-    }
-    
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
-    {
-        if (segue.identifier == "EmbedSegue")
-        {
-            var page = segue.destinationViewController as! IMercuryPage
-            
-            page.app = self
-            
-            _pages.push(page)
-        }
-    }
-        
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    */
+   
 }
 
 struct Stack<Element>
 {
-    var items = [Element]()
-    
-    mutating func push(item: Element)
-    {
-        items.append(item)
-    }
-    
-    mutating func pop() -> Element
-    {
-        return items.removeLast()
-    }
-    
-    func peek() -> Element
-    {
-        return items.last!
-    }
+   var items = [Element]()
+   
+   mutating func push(item: Element)
+   {
+      items.append(item)
+   }
+   
+   mutating func pop() -> Element
+   {
+      return items.removeLast()
+   }
+   
+   func peek() -> Element
+   {
+      return items.last!
+   }
 }
 
 
