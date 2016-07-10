f9936136aeaeb40f44df2cfefdd188f3f5f1f871
stephen eshelman <objectthink@comcast.net>
2016-07-10 07:49:37 -0400

    fetch name, location, and serial number in network page

--- NetworkPage.swift
+++ NetworkPage.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
+class GetSerialNumber:MercuryGet
+{
+   override init()
+   {
+      super.init()
+      self.subCommandId = 0x00000002
+   }
+}
+
+class GetName:MercuryGet
+{
+   override init()
+   {
+      super.init()
+      self.subCommandId = 0x00000003
+   }
+}
+
+class GetLocation:MercuryGet
+{
+   override init()
+   {
+      super.init()
+      self.subCommandId = 0x00000004
+   }
+}
+
 class NetworkPage: UIViewController, IMercuryPage
 {
+   @IBOutlet var _serialNumber: UILabel!
+   @IBOutlet var _nameText: UITextField!
+   @IBOutlet var _locationText: UITextField!
+   
    var _app:IMercuryApp!
    var app:IMercuryApp
       {
@@ -28,6 +59,51 @@ class NetworkPage: UIViewController, IMercuryPage
       super.viewDidLoad()
       
       // Do any additional setup after loading the view.
+      app.instrument.sendCommand(GetSerialNumber())
+      {(response)-> Void in
+         
+         if let str = NSString(data: response.bytes, encoding: NSUTF8StringEncoding) as? String
+         {
+            dispatch_async(dispatch_get_main_queue(),
+            { () -> Void in
+               self._serialNumber.text = str
+            })
+         }
+         else
+         {
+            print("not a valid UTF-8 sequence")
+         }
+      }
+      
+      app.instrument.sendCommand(GetName())
+      { (response) -> Void in
+         if let str = NSString(data: response.bytes, encoding: NSUTF8StringEncoding) as? String
+         {
+            dispatch_async(dispatch_get_main_queue(),
+            { () -> Void in
+               self._nameText.text = self.mercuryStringFixup(str)
+            })
+         }
+         else
+         {
+            print("not a valid UTF-8 sequence")
+         }
+      }
+      
+      app.instrument.sendCommand(GetLocation())
+      { (response) -> Void in
+         if let str = NSString(data: response.bytes, encoding: NSASCIIStringEncoding) as? String
+         {
+            dispatch_async(dispatch_get_main_queue(),
+            { () -> Void in
+               self._locationText.text = self.mercuryStringFixup(str)
+            })
+         }
+         else
+         {
+            print("not a valid UTF-8 sequence")
+         }
+      }
    }
    
    override func didReceiveMemoryWarning()
@@ -36,6 +112,20 @@ class NetworkPage: UIViewController, IMercuryPage
       // Dispose of any resources that can be recreated.
    }
    
+   func mercuryStringFixup(s:String)->String
+   {
+      var sOut:String = String()
+      
+      for character in s.characters
+      {
+         if character != "\0"
+         {
+            sOut.append(character)
+         }
+      }
+      
+      return sOut
+   }
    
    /*
     // MARK: - Navigation
